6abeb737603d9ffe3a1269e7eb7f604f9c90575e
fathima-shamsu <54524127+fathima-shamsu@users.noreply.github.com>
2020-05-27 13:09:31 +0530

    Update Toolbox.js

--- react/features/toolbox/components/native/Toolbox.js
+++ react/features/toolbox/components/native/Toolbox.js
@@ -117,9 +117,9 @@ class Toolbox extends PureComponent<Props> {
                 <VideoMuteButton
                     styles = { buttonStyles }
                     toggledStyles = { toggledButtonStyles } />
-                <OverflowMenuButton
-                    styles = { buttonStylesBorderless }
-                    toggledStyles = { toggledButtonStyles } />
+//                 <OverflowMenuButton
+//                     styles = { buttonStylesBorderless }
+//                     toggledStyles = { toggledButtonStyles } />
             </View>
         );
     }
